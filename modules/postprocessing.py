0b59ac856202860141818a3a12610abd7c20b184
Ahmedkhalifa1999 <ahmedkhalifa1999@gmail.com>
2022-04-27 15:36:21 +0200

    Module Notebooks 2

---
+++ modules/postprocessing.py
@@ -0,0 +1,8 @@
+import asn1crypto
+import numpy as np
+import cv2 as cv
+
+def postprocess(image):
+    # get lane metrics from bianry lane image
+    # add lane metrics as text to image
+    pass
---
+++ modules/region_of_interest.py
@@ -0,0 +1,6 @@
+import numpy as np
+import cv2 as cv
+
+def perspective_transform(image):
+    # Perspective Transform
+    pass

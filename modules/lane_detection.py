d5785946c059c932324212dda73214d8118fd141
Ahmedkhalifa1999 <ahmedkhalifa1999@gmail.com>
2022-04-27 20:50:08 +0200

    perspective transform update

--- modules/lane_detection.py
+++ modules/lane_detection.py
@@ -1,6 +1,8 @@
+import numpy as np
 import cv2 as cv
 
 def detect_lanes(image):
+    
     h, s, v = cv.split(image)
     _, white_s_thresholded = cv.threshold(s, 5, 255, cv.THRESH_BINARY_INV)
     _, white_v_thresholded = cv.threshold(v, 200, 255, cv.THRESH_BINARY)
@@ -9,4 +11,8 @@ def detect_lanes(image):
     _, yellow_v_thresholded = cv.threshold(s, 100, 255, cv.THRESH_BINARY)
     white_mask = cv.bitwise_and(white_s_thresholded, white_v_thresholded)
     yellow_mask = cv.bitwise_and(cv.bitwise_and(yellow_h_thresholded, yellow_s_thresholded), yellow_v_thresholded)
-    return cv.bitwise_or(white_mask, yellow_mask)
+    lane_mask = cv.bitwise_or(white_mask, yellow_mask)
+    # lane_mask[0:100, :] = 0
+    # lane_mask[400:500, :] = 0
+    return lane_mask
+        

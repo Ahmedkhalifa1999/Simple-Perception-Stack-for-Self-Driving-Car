4171d877326e8c33fbc94535b495257755f9c958
Ahmedkhalifa1999 <ahmedkhalifa1999@gmail.com>
2022-04-27 17:12:54 +0200

    region of interest

--- modules/region_of_interest.py
+++ modules/region_of_interest.py
@@ -2,5 +2,15 @@ import numpy as np
 import cv2 as cv
 
 def perspective_transform(image):
-    # Perspective Transform
-    pass
+    src = np.float32([[375, 480],
+                    [905, 480],
+                    [1811, 685],
+                    [-531, 685]])
+    dst = np.float32([[0, 0],
+                    [500, 0],
+                    [500, 600],
+                    [0, 600]])
+    M = cv.getPerspectiveTransform(src, dst)
+    Minv = cv.getPerspectiveTransform(dst, src)
+    warped = cv.warpPerspective(image, M, (500, 600))
+    return warped, Minv

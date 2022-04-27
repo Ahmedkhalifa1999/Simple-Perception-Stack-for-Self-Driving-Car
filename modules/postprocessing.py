a334a9ccb2828a7ac17625b1a432663b2fd7ff35
Ahmedkhalifa1999 <ahmedkhalifa1999@gmail.com>
2022-04-27 23:23:12 +0200

    Delete postprocessing.py

--- modules/postprocessing.py
+++
@@ -1,8 +0,0 @@
-import asn1crypto
-import numpy as np
-import cv2 as cv
-
-def postprocess(image, Minv):
-    # get lane metrics from bianry lane image
-    # add lane metrics as text to image
-    pass
